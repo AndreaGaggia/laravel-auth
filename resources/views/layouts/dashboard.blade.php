203c3c93b02fe68be90ecf1ae1ede0c75be8f284
AndreaGaggia <gaggiandrea@gmail.com>
2021-03-01 17:29:57 +0100

    delete method with modal

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -63,13 +63,16 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                                                                                 document.getElementById('logout-form').submit();">
+                                                                                                                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
+                                    <a class="dropdown-item" href="{{ route('admin.posts.index') }}">
+                                        Admin
+                                    </a>
                                 </div>
                             </li>
                         @endguest
